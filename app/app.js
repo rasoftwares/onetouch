84280580116f696e562de97feb302648fca85d0f
robinbennedict <robinbennedict@gmail.com>
2017-01-22 21:18:35 +0530

    cleaned up dynaform input template via js

--- app/app.js
+++ app/app.js
@@ -1,223 +1,97 @@
-angular.module('app', ['dynform'])
-  .controller('AppCtrl', ['$scope', function ($scope) {
-    $scope.Form = {
-      "fieldset-A": {
-        "type": "fieldset",
-        "label": "Company",
-        "fields": {
-          "companyname": {
-            "type": "text",
-            "label": "CompanyName",
-            "placeholder": "text"
-          },
-          "registration no":{
-            "type": "text",
-            "label": "Registration no",
-            "placeholder": "text"
-          },
-		    "title": {
-            "type": "text",
-            "label": "Title",
-            "placeholder": "title"
-          },
-          "Registration Year": {
-            "type": "date",
-            "label": "Registration Year",
-            "placeholder": "date"
-          },
-          "shortDescription": {
-            "type": "textarea",
-            "label": "ShortDescription",
-            "placeholder": "ShortDescription"
-          },
-     "coordinates-fieldset": {
-            "type": "fieldset",
-            "label": "Owner",
-        "fields": {
-              "OwnerName": {
-                "type": "text",
-                "label": "OwnerName",
-                "placeholder": "OwnerName",
-               },
-			    "photograph": {
-                "type": "text",
-                "label": "Image",
-				"placeholder": "Url",
-			    "source": ""
-               },
-          
-		  }
-			 
-          },
-	        		  
-        }      
-	 
-	  },
-  "fieldset-B": {
-            "type": "fieldset",
-            "label": "Contact",
-        "fields": {
-               "registeredAddress": {
-                "type": "textarea",
-                "label": "RegisteredAddress",
-                "placeholder": "registeredAddress",
-                },
-				"officeAddress": {
-                "type": "textarea",
-                "label": "OfficeAddress",
-                "placeholder": "officeAddress",
-                },
-					
-			  "landlineNumber": {
-                "type": "number",
-                "label": "LandlineNumber",
-                "placeholder": "LandlineNumber",
-                },
-			  "mobileNumber": {
-                "type": "number",
-                "label": "MobileNumber",
-                "placeholder": "mobileNumber",
-                },
-				"emailId": {
-                "type": "email",
-                "label": "EmailId",
-                "placeholder": "emailid"
-                },
-			       }
-          },
-	 
-		   
-	 "fieldset-c": {
-            "type": "fieldset",
-            "label": "website",
-        "fields": {
-             "website": {
-                "type": "text",
-                "label": "website",
-                "placeholder": "website"
-                },
-				 "aboutcompany": {
-                "type": "textarea",
-                "label": "AboutCompany",
-                "placeholder": "aboutcompany"
-                },
-				 "aboutBusiness": {
-                "type": "textarea",
-                "label": "AboutBusiness",
-                "placeholder": "aboutBusiness"
-                },
-				}
-			 	
-          },
-				
-    "fieldset-D": {
-            "type": "fieldset",
-            "label": "Gallary",
-         "fields": {
-             "name1": {
-                "type": "text",
-                "label": "Image",
-				"placeholder": "Url",
-                 "source": ""
-                },
-				  "name2": {
-                "type": "text",
-                "label": "Image",
-				"placeholder": "Url",
-                 "source": ""
-                },
-			      "name3": {
-                "type": "text",
-                "label": "Image",
-				"placeholder": "Url",
-                 "source": ""
-                },
-				}
-        },
-           
-	"fieldset-E": {
-            "type": "fieldset",
-            "label": "Product",
-            "fields": {
-             "productname": {
-                "type": "text",
-                "label": "ProductName",
-                 "placeholder": "productname"
-                },
-				 "price": {
-                "type": "number",
-                "label": "Price",
-                 "placeholder": "price"
-                },
-				 "Discount": {
-                "type": "number",
-                "label": "Discount",
-                 "placeholder": "discount"
-                },
-				 }
-          },
-	"fieldset-F": {
-            "type": "fieldset",
-            "label": "Services",
-            "fields": {
-             "productname": {
-                "type": "text",
-                "label": "ServiceName",
-                 "placeholder": "servicename"
-                },
-				 "description": {
-                "type": "textarea",
-                "label": "description",
-                 "placeholder": "description"
-			
-                },
-				"brandImageIcon": {
-                "type": "text",
-                "label": "brandImageIcon",
-				"placeholder": "Url",
-                 "source":""
-                },
-				"copyright": {
-                "type": "text",
-                "label": "Copyright",
-                "placeholder": "copyright"
-                },
-				 "applicationType": {
-                "type": "text",
-                "label": "ApplicationType",
-                 "placeholder": "applicationType"
-                },
-				 "category": {
-                "type": "text",
-                "label": "Category",
-                 "placeholder": "category"
-                },
-				 "type": {
-                "type": "text",
-                "label": "Type",
-                 "placeholder": "type"
-                },
-				"item": {
-                "type": "text",
-                "label": "Item",
-                 "placeholder": "Item"
-                },
-				"summary": {
-                "type": "textarea",
-                "label": "Summary",
-                "placeholder": "summary"
-				 
-                },
-	}
-	   },
-      "submit": {
-        "type": "submit",
-        "label": "submit"
-      }
-   
+var app = angular.module('app', ['dynform']);
+
+app.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
+    var environment = "dev";
+    var dataStore = "request";
+    var authKey = "ve8PdopndzS3yD35SMF6KAd4VKpHQuxUotXNeHGw";
+    var appURL = "https://onetouch-d52d4.firebaseio.com/"+ environment + "/" + dataStore +".json?auth="+ authKey;
+
+    
+
+    $scope.formData = { };
+
+    $scope.fromTemplate = templateData;
+
+
+    $scope.processForm = function () {
+        /* Handle the form submission... */
+        console.log("Processing Submit Action");
+        console.log($scope.formData);
     };
-    $scope.stdFormData = {};
-   // $scope.urlFormData = {};
+
+
+//data to object (json):
+  $scope.formDataToObject = function (elForm) {
+    console.log("---->" + elForm);
+
+  if (!elForm instanceof Element) return;
+  var fields = elForm.querySelectorAll('input, select, textarea'),
+    a = {};
+  for (var i=0, imax=fields.length; i<imax; ++i) {
+    var field = fields[i],
+      sKey = field.name || field.id;
+    if (field.type==='button' || field.type==='image' || field.type==='submit' || !sKey) continue;
+    switch (field.type) {
+      case 'checkbox':
+        a[sKey] = +field.checked;
+        break;
+      case 'radio':
+        if (a[sKey]===undefined) o[sKey] = '';
+        if (field.checked) a[sKey] = field.value;
+        break;
+      case 'select-multiple':
+        var b = [];
+        for (var j=0, jmax=field.options.length; j<jmax; ++j) {
+          if (field.options[j].selected) a.push(field.options[j].value);
+        }
+        a[sKey] = a;
+        break;
+      default:
+        a[sKey] = field.value;
+    }
+  }
+    console.log("Data :" + a);
+
+     //var submissionsRef = new Firebase('https://form-example-22b8c.firebaseio.com/submissions');
+
+   //data format:
+   var data = a;
+
+   $http({
+      method: 'POST',
+      url: appURL,
+      data: data
+    }).then(function successCallback(response) {
+        // this callback will be called asynchronously
+        // when the response is available
+        console.log("Saved data successfully" + response.status);
+      }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        console.log("Failed to save data" + response.status);
+        console.log("ERROR" + response.error);
+
+      });
+
+
+  /*submissionsRef.push(data, function(err) {
+    if (err) {
+        window.alert('Error when submitting data: ' + err.message);
+    }
+   });
+  window.alert('Submitted answers: \n' + JSON.stringify(data, null, 2));
+ */
+    //console.log('Form data:' + JSON.stringify(a, null, 2));
+    Form.reset();
+
+    return a;
+  
+ }
+
+
+
+
+	
   }])
   .filter('pretty', function() {
     return function (input) {
@@ -232,3 +106,79 @@ angular.module('app', ['dynform'])
       return angular.toJson(temp, true);
     };
   });
+  
+  
+
+ 
+ 
+ //view data:
+ var aFormData = {
+  "companyName": "rablion",
+  "registrationNo": "1234",
+  "title": "catalog",
+  "RegistrationYear": "2016",
+  "shortDescription": "software",
+  "OwnerName": "ownerer",
+  "photograph": "",
+  "registeredAddress": "kelambakkam",
+  "officeAddress": "kelambakkam",
+  "landlineNumber": "03334444",
+  "mobileNumber": "1234567890",
+  "emailId": "mail@gamil.com",
+  "website": "www.rablion.com",
+  "aboutcompany": "it",
+  "aboutBusiness": "it",
+  "name1": "",
+  "name2": "",
+  "name3": "",
+  "productname": "catalog",
+  "price": "1000",
+  "Discount": "20",
+  "Servicename": "avrs",
+  "description": "shopping",
+  "brandImageIcon": "",
+  "copyright": "@avrs",
+  "applicationType": "moblie app",
+  "category": "textile",
+  "type": "product",
+  "item": "all",
+  "summary": "textil business"
+};
+
+
+
+
+//view data from json :
+
+function isObject(arg) {
+  return Object.prototype.toString.call(arg)==='[object Object]';
+}
+
+function populateForm(a) {
+  if (!isObject(a)) return;
+  for (var i in a) {
+    var el = document.getElementById(i) || document.querySelector('[name=' + i + ']');
+    if (el.type==='radio') el = document.querySelectorAll('[name=' + i + ']');
+    switch (typeof a[i]) {
+      case 'number':
+        el.checked = a[i];
+        break;
+      case 'object':
+        if (el.options && a[i] instanceof Array) {
+          for (var j=0, jmax=el.options.length; j<jmax; ++j) {
+            if (a[i].indexOf(el.options[j].value)>-1) el.options[j].selected = true;
+          }
+        }
+        break;
+      default:
+        if (el instanceof NodeList) {
+          for (var j=0, jmax=el.length; j<jmax; ++j) {
+            if (el[j].value===a[i]) el[j].checked = true;
+          }
+        } else {
+          el.value = a[i];
+        }
+    }
+  }
+
+}
---
+++ app/genericTemplate.js
@@ -0,0 +1,216 @@
+var templateData = {
+      "fieldset-A": {
+        "type": "fieldset",
+        "label": "Company",
+        "fields": {
+          "companyName": {
+            "type": "text",
+            "label": "CompanyName",
+            "placeholder": "text"
+          },
+          "registrationNo":{
+            "type": "text",
+            "label": "RegistrationNo",
+            "placeholder": "text"
+          },
+		    "title": {
+            "type": "text",
+            "label": "Title",
+            "placeholder": "title"
+          },
+          "RegistrationYear": {
+            "type": "date",
+            "label": "RegistrationYear",
+            "placeholder": "date"
+          },
+          "shortDescription": {
+            "type": "textarea",
+            "label": "ShortDescription",
+            "placeholder": "ShortDescription"
+          },
+     "coordinates-fieldset": {
+            "type": "fieldset",
+            "label": "Owner",
+        "fields": {
+              "OwnerName": {
+                "type": "text",
+                "label": "OwnerName",
+                "placeholder": "OwnerName"
+               },
+			    "photograph": {
+				
+				"id":"file",
+                "type": "file",
+                "label": "Photograph",
+				"placeholder": "Url",
+				"alt":"fileDispalyarea"
+				
+				} 
+		  }
+			 
+          }
+	        		  
+        }      
+	 
+	  },
+  "fieldset-B": {
+            "type": "fieldset",
+            "label": "Contact",
+        "fields": {
+               "registeredAddress": {
+                "type": "textarea",
+                "label": "RegisteredAddress",
+                "placeholder": "registeredAddress"
+                },
+				"officeAddress": {
+                "type": "textarea",
+                "label": "OfficeAddress",
+                "placeholder": "officeAddress"
+                },
+					
+			  "landlineNumber": {
+                "type": "number",
+                "label": "LandlineNumber",
+                "placeholder": "LandlineNumber"
+                },
+			  "mobileNumber": {
+                "type": "number",
+                "label": "MobileNumber",
+                "placeholder": "mobileNumber"
+                },
+				"emailId": {
+                "type": "email",
+                "label": "EmailId",
+                "placeholder": "emailid"
+                }
+			       }
+          },
+	 
+		   
+	 "fieldset-c": {
+            "type": "fieldset",
+            "label": "Website",
+        "fields": {
+             "website": {
+                "type": "text",
+                "label": "Website",
+                "placeholder": "website"
+                },
+				 "aboutcompany": {
+                "type": "textarea",
+                "label": "AboutCompany",
+                "placeholder": "aboutcompany"
+                },
+				 "aboutBusiness": {
+                "type": "textarea",
+                "label": "AboutBusiness",
+                "placeholder": "aboutBusiness"
+                }
+				}
+			 	
+          },
+				
+    "fieldset-D": {
+            "type": "fieldset",
+            "label": "Gallary",
+         "fields": {
+             "name1": {
+                "type": "file",
+                "label": "Image",
+				"placeholder": "Url",
+                 "source": ""
+                },
+				  "name2": {
+                "type": "file",
+                "label": "Image",
+				"placeholder": "Url",
+                 "source": ""
+				 
+                },
+			      "name3": {
+                "type": "file",
+                "label": "Image",
+				"placeholder": "Url",
+                 "source": ""
+                }
+				}
+        },
+           
+	"fieldset-E": {
+            "type": "fieldset",
+            "label": "Product",
+            "fields": {
+             "productname": {
+                "type": "text",
+                "label": "ProductName",
+                 "placeholder": "productname"
+                },
+				 "price": {
+                "type": "number",
+                "label": "Price",
+                 "placeholder": "price"
+                },
+				 "Discount": {
+                "type": "number",
+                "label": "Discount",
+                 "placeholder": "discount"
+                }
+				 }
+          },
+	"fieldset-F": {
+            "type": "fieldset",
+            "label": "Services",
+            "fields": {
+             "Servicename": {
+                "type": "text",
+                "label": "ServiceName",
+                 "placeholder": "servicename"
+                },
+				 "description": {
+                "type": "textarea",
+                "label": "Description",
+                 "placeholder": "description"
+			
+                },
+				"brandImageIcon": {
+				"id":"brandImageIcon",
+			    "name":"brandImageIcon",
+                "type": "file",
+                "label": "BrandImageIcon",
+				"placeholder": "Url",
+                 "source":""
+                },
+				"copyright": {
+                "type": "text",
+                "label": "Copyright",
+                "placeholder": "copyright"
+                },
+				 "applicationType": {
+                "type": "text",
+                "label": "ApplicationType",
+                 "placeholder": "applicationType"
+                },
+				 "category": {
+                "type": "text",
+                "label": "Category",
+                 "placeholder": "category"
+                },
+				 "type": {
+                "type": "text",
+                "label": "Type",
+                 "placeholder": "type"
+                },
+				"item": {
+                "type": "text",
+                "label": "Item",
+                 "placeholder": "Item"
+                },
+				"summary": {
+                "type": "textarea",
+                "label": "Summary",
+                 "placeholder": "summary"
+				 
+                }
+	       }
+	   }
+  }
