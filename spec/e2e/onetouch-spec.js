a2bb84c1c1a3c0eb932f25eaaa0b4b5fe19cfa88
premraja <veerabadraraja@gmail.com>
2017-02-17 12:33:43 +0530

    added details in onetouch-spec.js

--- spec/e2e/onetouch-spec.js
+++ spec/e2e/onetouch-spec.js
@@ -17,7 +17,18 @@ describe('onetouch App', function() {
     element(by.name('company.title')).sendKeys('Rablion softwares private limited');
     element(by.name('company.registrationNo')).sendKeys(12345);
     element(by.name('company.date')).sendKeys('01/01/2015');
+    element(by.name('company.owner.name')).sendKeys('Rajesh');
+    //  element(by.name('company.owner.photgraph')).sendKeys('');
+    element(by.name('company.shortDescription')).sendKeys('Software Development');
 
+    element(by.name('contact.registrationAddress')).sendKeys('B 102, SSPDL CRESCENT APARTMENTS 1/278, VANDALLUR ROAD, KELAMBAKKAM CHENNAI - 603103');
+    element(by.name('contact.officeAddress')).sendKeys('B 102, SSPDL CRESCENT APARTMENTS 1/278, VANDALLUR ROAD, KELAMBAKKAM CHENNAI - 603103');
+    element(by.name('contact.landlineNumber')).sendKeys('044-22456896');
+    element(by.name('contact.mobileNumber')).sendKeys('9791189231');
+    element(by.name('contact.emailId')).sendKeys('info@rasoftwares.com');
+
+    element(by.name('website.website')).sendKeys('www.rablion.com');
+    element(by.name('website.aboutCompany')).sendKeys('Software Development and career Development');
 
     element(by.id('submit_btn')).click();
   });
